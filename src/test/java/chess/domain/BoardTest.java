7776988a08ebb81e61a906e8e721d6822b3ccc98
CarlinChow <carlin_chow@sfu.ca>
2023-06-29 20:03:59 -0700

    added Test folder

---
+++ src/test/java/chess/domain/BoardTest.java
@@ -0,0 +1,11 @@
+package chess.domain;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class BoardTest {
+    @Test
+    public void testBoardLength(){
+        assertTrue(true);
+    }
+}
