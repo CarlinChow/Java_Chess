6ae3bc8b67f5c9f78281139ba6211296328a7de7
CarlinChow <carlin_chow@sfu.ca>
2023-07-03 20:31:34 -0700

    small fixes

--- src/main/java/chess/domain/pieces/Pawn.java
+++ src/main/java/chess/domain/pieces/Pawn.java
@@ -14,36 +14,34 @@ public class Pawn extends Piece{
     public boolean canMove(Board board, Spot start, Spot end){
         int verticalMovement = start.getRow() - end.getRow();
         int horizontalMovement = start.getColumn() - end.getColumn();
-        if(Math.abs(verticalMovement + horizontalMovement) > 2 || Math.abs(horizontalMovement) > 1 || verticalMovement == 0){
-            // check if piece making an illegal move,
-            System.out.println("pawn is making a illegal move");
+        if (Math.abs(verticalMovement + horizontalMovement) > 2 ||
+                Math.abs(horizontalMovement) > 1 ||
+                verticalMovement == 0)
+        {
+            // check if piece is making an illegal move,
             return false;
         }
         if((this.isWhite() && verticalMovement < 0) || (!this.isWhite() && verticalMovement > 0)){
             // check if piece is moving backwards
-            System.out.println("Pawn is moving backwards");
             return false;
         }
 
         int startingRow = this.isWhite() ? 6 : 1;
         if(Math.abs(verticalMovement) == 2 && start.getRow() == startingRow){
             // handle moving forward twice
-            int diff = 1;
-            if(this.isWhite()){
-                diff = -1;
-            }
+            int diff = this.isWhite() ? -1 : 1;
             // check if spots in front are empty
             Spot spot = board.getSpotAt(start.getRow() + diff, start.getColumn());
-            if(spot.isEmpty() && end.isEmpty()){
-                return true;
-            }
-            return false;
+            return spot.isEmpty() && end.isEmpty();
         }
-        if(horizontalMovement == 0){
+        if(horizontalMovement == 0 && Math.abs(verticalMovement) == 1){
             return end.isEmpty();
         }
         // check if attack is legal
-        return !end.isEmpty() && end.getPiece().isWhite() != this.isWhite();
+        if(Math.abs(verticalMovement) == 1 && Math.abs(horizontalMovement) == 1){
+            return !end.isEmpty() && end.getPiece().isWhite() != this.isWhite();
+        }
+        return false;
     }
 
     @Override
