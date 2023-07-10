e4bd839080d40e5938a099fb8c95ea3977a9b938
CarlinChow <carlin_chow@sfu.ca>
2023-07-10 10:47:05 -0700

    added Queen Class

---
+++ src/main/java/chess/domain/pieces/Queen.java
@@ -0,0 +1,19 @@
+package chess.domain.pieces;
+
+import chess.domain.*;
+
+public class Queen extends Piece{
+    public Queen(boolean white){
+        super(white);
+    }
+
+    @Override
+    public boolean canMove(Board board, Spot start, Spot end) {
+        Piece bishop = new Bishop(this.isWhite());
+        Piece rook = new Rook(this.isWhite());
+        return bishop.canMove(board, start, end) || rook.canMove(board, start, end);
+    }
+    public String toString(){
+        return "Q";
+    }
+}
