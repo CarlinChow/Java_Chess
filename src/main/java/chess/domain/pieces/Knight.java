b95d852a77ba926041bb0dc118c7ede70e7ac09e
CarlinChow <carlin_chow@sfu.ca>
2023-07-11 17:02:45 -0700

    added Knight class

---
+++ src/main/java/chess/domain/pieces/Knight.java
@@ -0,0 +1,24 @@
+package chess.domain.pieces;
+
+import chess.domain.*;
+import java.lang.Math;
+
+public class Knight extends Piece{
+    public Knight(boolean white){
+        super(white);
+    }
+
+    public boolean canMove(Board board, Spot start, Spot end){
+        int verticalMovement = start.getRow() + end.getRow();
+        int horizontalMovement = start.getColumn() + end.getColumn();
+        if(Math.abs(verticalMovement * horizontalMovement) == 2){
+            return end.isEmpty() || end.getPiece().isWhite() != this.isWhite();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return "K";
+    }
+}
