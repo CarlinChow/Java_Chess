6890b111fc245c33284d595b93725ab53b7e4734
CarlinChow <carlin_chow@sfu.ca>
2023-07-26 10:30:45 -0700

    added moveList, move class

---
+++ src/main/java/chess/logic/Move.java
@@ -0,0 +1,37 @@
+package chess.logic;
+
+import chess.domain.*;
+
+public class Move {
+    private final Piece piece;
+    private final Spot start;
+    private final Spot end;
+    private final Piece capturedPiece;
+
+    public Move(Piece piece, Spot start, Spot end, Piece capturedPiece){
+        this.piece = piece;
+        this.start = start;
+        this.end = end;
+        this.capturedPiece = capturedPiece;
+    }
+
+    public Move(Piece piece, Spot start, Spot end){
+        this(piece, start, end, null);
+    }
+
+    public Piece getPiece(){
+        return this.piece;
+    }
+
+    public Spot getStart(){
+        return this.start;
+    }
+
+    public Spot getEnd(){
+        return this.end;
+    }
+
+    public Piece getCapturedPiece(){
+        return this.capturedPiece;
+    }
+}
