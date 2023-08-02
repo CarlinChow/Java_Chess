b91551cb76b652c0713605a527376509f0537b4d
CarlinChow <carlin_chow@sfu.ca>
2023-08-02 16:14:24 -0700

    finsihed Knight.getMoves() and test cases

--- src/main/java/chess/domain/pieces/Knight.java
+++ src/main/java/chess/domain/pieces/Knight.java
@@ -4,6 +4,9 @@ import chess.domain.*;
 import static java.lang.Math.abs;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Knight extends Piece{
     public Knight(boolean white){
@@ -23,6 +26,30 @@ public class Knight extends Piece{
     @Override
     public Set<Spot> getMoves(Board board){
         Set<Spot> moves = new HashSet<>();
+        Spot currSpot = this.getSpot();
+        int currRow = currSpot.getRow();
+        int currCol = currSpot.getColumn();
+        // construct array of indices for all possible moves, 8 total in [row,col] format
+        List<int[]> moveIndices = new LinkedList<>();
+        moveIndices.add(new int[]{currRow + 2, currCol - 1});
+        moveIndices.add(new int[]{currRow + 1, currCol - 2});
+        moveIndices.add(new int[]{currRow - 1, currCol - 2});
+        moveIndices.add(new int[]{currRow - 2, currCol - 1});
+        moveIndices.add(new int[]{currRow - 2, currCol + 1});
+        moveIndices.add(new int[]{currRow - 1, currCol + 2});
+        moveIndices.add(new int[]{currRow + 1, currCol + 2});
+        moveIndices.add(new int[]{currRow + 2, currCol + 1});
+        // filter out illegal moves
+        moveIndices = moveIndices
+                        .stream()
+                        .filter(arr -> arr[0] >= 0 && arr[0] < 8 && arr[1] >= 0 && arr[1] < 8)
+                        .collect(Collectors.toList());
+        for(int[] index : moveIndices){
+            Spot moveSpot = board.getSpotAt(index[0], index[1]);
+            if(this.canMove(board, moveSpot)){
+                moves.add(moveSpot);
+            }
+        }
         return moves;
     }
 
--- src/test/java/chess/domain/pieces/KnightTest.java
+++ src/test/java/chess/domain/pieces/KnightTest.java
@@ -2,6 +2,8 @@ package chess.domain.pieces;
 
 import chess.domain.*;
 import org.junit.Test;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class KnightTest {
@@ -87,5 +89,79 @@ public class KnightTest {
         assertFalse(whiteKnight1.canMove(board, board.getSpotAt("d2")));
         assertFalse(whiteKnight2.canMove(board, board.getSpotAt("f3")));
         assertFalse(whiteKnight2.canMove(board, board.getSpotAt("e2")));
+    }
+
+    @Test
+    public void testGetMoves(){
+        Board board = new Board();
+        this.setUp(board);
+        Piece blackQueenKnight = board.getSpotAt("b8").getPiece();
+        Piece blackKingKnight = board.getSpotAt("g8").getPiece();
+        Piece whiteQueenKnight = board.getSpotAt("b1").getPiece();
+        Piece whiteKingKnight = board.getSpotAt("g1").getPiece();
+
+        Set<Spot> blackQueenKnightMoves = blackQueenKnight.getMoves(board);
+        Set<Spot> blackKingKnightMoves = blackKingKnight.getMoves(board);
+        Set<Spot> whiteQueenKnightMoves = whiteQueenKnight.getMoves(board);
+        Set<Spot> whiteKingKnightMoves = whiteKingKnight.getMoves(board);
+
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("a6")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("c6")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("d7")));
+        assertEquals(3, blackQueenKnightMoves.size());
+        assertTrue(blackKingKnightMoves.contains(board.getSpotAt("e7")));
+        assertTrue(blackKingKnightMoves.contains(board.getSpotAt("f6")));
+        assertTrue(blackKingKnightMoves.contains(board.getSpotAt("h6")));
+        assertEquals(3, blackKingKnightMoves.size());
+        assertTrue(whiteQueenKnightMoves.contains(board.getSpotAt("a3")));
+        assertTrue(whiteQueenKnightMoves.contains(board.getSpotAt("c3")));
+        assertTrue(whiteQueenKnightMoves.contains(board.getSpotAt("d2")));
+        assertEquals(3, whiteQueenKnightMoves.size());
+        assertTrue(whiteKingKnightMoves.contains(board.getSpotAt("e2")));
+        assertTrue(whiteKingKnightMoves.contains(board.getSpotAt("f3")));
+        assertTrue(whiteKingKnightMoves.contains(board.getSpotAt("h3")));
+        assertEquals(3, whiteKingKnightMoves.size());
+
+        // adding interference
+        board.getSpotAt("d7").setPiece(new Pawn(false));
+        board.getSpotAt("e7").setPiece(new Pawn(false));
+        board.getSpotAt("a6").setPiece(new Pawn(true));
+        board.getSpotAt("f6").setPiece(new Pawn(true));
+        board.getSpotAt("d2").setPiece(new Pawn(true));
+        board.getSpotAt("e2").setPiece(new Pawn(true));
+        board.getSpotAt("a3").setPiece(new Pawn(false));
+        board.getSpotAt("f3").setPiece(new Pawn(false));
+
+        blackQueenKnightMoves = blackQueenKnight.getMoves(board);
+        blackKingKnightMoves = blackKingKnight.getMoves(board);
+        whiteQueenKnightMoves = whiteQueenKnight.getMoves(board);
+        whiteKingKnightMoves = whiteKingKnight.getMoves(board);
+
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("a6")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("c6")));
+        assertEquals(2, blackQueenKnightMoves.size());
+        assertTrue(blackKingKnightMoves.contains(board.getSpotAt("f6")));
+        assertTrue(blackKingKnightMoves.contains(board.getSpotAt("h6")));
+        assertEquals(2, blackKingKnightMoves.size());
+        assertTrue(whiteQueenKnightMoves.contains(board.getSpotAt("a3")));
+        assertTrue(whiteQueenKnightMoves.contains(board.getSpotAt("c3")));
+        assertEquals(2, whiteQueenKnightMoves.size());
+        assertTrue(whiteKingKnightMoves.contains(board.getSpotAt("f3")));
+        assertTrue(whiteKingKnightMoves.contains(board.getSpotAt("h3")));
+        assertEquals(2, whiteKingKnightMoves.size());
+
+        // moving black queen side knight to center of board
+        board.getSpotAt("b8").removePiece();
+        board.getSpotAt("c6").setPiece(blackQueenKnight);
+        blackQueenKnightMoves = blackQueenKnight.getMoves(board);
+
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("b8")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("d8")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("a7")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("a5")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("b4")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("d4")));
+        assertTrue(blackQueenKnightMoves.contains(board.getSpotAt("e5")));
+        assertEquals(7, blackQueenKnightMoves.size());
     }
 }
